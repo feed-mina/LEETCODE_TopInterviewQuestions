5f299ad565bdc6138246c0a51dc844e00e130e57
feed-mina <myelin24@naver.com>
2026-01-06 10:44:09 +0900

    sorted()와 .sort()의 차이

--- SortingandSearching/MergeSortedArray.py
+++ SortingandSearching/MergeSortedArray.py
@@ -12,8 +12,14 @@ class Solution:
         print("n", len(nums2))
 
         nums1[m:] = nums2
+        print("m",len(nums1[m:]) )
+        #  sorted(nums1): 새로운 리스트를 생성 (Return new list), 정렬된 새로운 리스트를 반환, 원본이 그대로 유지됨
+        print("sorted(nums1)",sorted(nums1))
+        print("new_nums1", nums1)
+        # nums1.sort() 리스트 자체를 수정(in-place), 결과값은 None을 반환, 원본이 바뀜
         nums1.sort()
         print("nums1", nums1)
+
         return nums1
 
 
@@ -21,5 +27,5 @@ class Solution:
         
 if __name__ == "__main__":
     sol = Solution()
-    result = sol.merge([1,2,3,0,0,0],3,[1,2,3,0,0,0],3)
+    result = sol.merge([1,2,3,0,0,0],3,[2,5,6],3)
     print("result", result)
